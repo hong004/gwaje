63816747f0217588859502f5fd6ff2f0d5968d1f
hong004 <127911982+hong004@users.noreply.github.com>
2023-05-17 10:51:58 +0900

    Add files via upload

---
+++ 과제.py
@@ -0,0 +1,10 @@
+if computer_choice == 0 and my_choice == 2:
+ print("컴퓨터가 이겼다")
+elif my_choice == 0 and computer_choice == 2:
+ print("내가 이겼다!")
+elif computer_choice > my_choice:
+ print("컴퓨터가 이겼다")
+elif my_choice == computer_choice:
+ print("비겼다!")
+elif my_choice > computer_choice:
+ print("내가 이겼다!")
